b9316803c116063298fdeff2829eb0afcd9355b0
jasonbringle <jasonbringle@gmail.com>
2020-09-27 18:12:54 -0700

    ring buffer implemented

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.arr = []
+        self.current = 0
 
     def append(self, item):
-        pass
+        if len(self.arr) < self.capacity:
+            self.arr.append(item)
+        else:
+            self.arr[self.current] = item
+            self.current = (self.current + 1) % self.capacity
+
 
     def get(self):
-        pass
+        return self.arr
+        
